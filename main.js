1b810f3c9e89a8b6892f5533f29f7c1badb7049f
julius oh <juliusoh@gmail.com>
2020-06-16 15:55:20 -0700

    add page header com

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // 
 //   "apiKey": "olYtLdiN",
 //   "message": "User for Julius Oh created. Write down your API Key, you will need it for the rest of this project."
+var header = document.querySelector('container');
+var headerApp = new PageHeader(header);
 var table = document.querySelector('table');
 var gradeTable = new GradeTable(table);
 console.log(gradeTable);
